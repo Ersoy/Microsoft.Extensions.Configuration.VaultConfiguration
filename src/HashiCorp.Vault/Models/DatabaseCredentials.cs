4fff94adaf7d01ebac68cb8fb4a92a848d42bac4
Ersoy Hasanoglu <ersoyha@hotmail.com>
2017-10-25 20:00:03 +0200

    add database backend specific model

---
+++ src/HashiCorp.Vault/Models/DatabaseCredentials.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace HashiCorp.Vault.Models {
+
+    [DataContract]
+    public class DatabaseCredentials {
+
+        [DataMember(Name = "username")]
+        public string UserName { get; set; }
+
+        [DataMember(Name = "password")]
+        public string Password { get; set; }
+
+    }
+
+}
